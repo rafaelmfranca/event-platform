c703267f27333a4ffd6513312f8062036523f44c
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-06-22 16:22:41 -0300

    feat: add sidebar and lesson components

---
+++ src/components/Lesson/index.tsx
@@ -0,0 +1,45 @@
+import { format, isPast } from 'date-fns';
+import ptBr from 'date-fns/locale/pt-BR';
+import { CheckCircle, Lock } from 'phosphor-react';
+
+interface ILessonProps {
+  title: string;
+  slug: string;
+  availableAt: Date;
+  type: 'live' | 'class';
+}
+
+export default function Lesson({ title, slug, availableAt, type }: ILessonProps) {
+  const isLessonAvailable = isPast(availableAt);
+  const availableDateFormatted = format(availableAt, "EEEE' • 'd' de 'MMMM' • 'k'h'mm", {
+    locale: ptBr,
+  });
+
+  return (
+    <a href={slug}>
+      <span className="text-gray-300">{availableDateFormatted}</span>
+
+      <div className="p-4 mt-2 border border-gray-500 rounded">
+        <header className="flex items-center justify-between">
+          {isLessonAvailable ? (
+            <span className="flex items-center gap-2 text-sm font-medium text-cyan-500">
+              <CheckCircle size={20} />
+              Conteúdo liberado
+            </span>
+          ) : (
+            <span className="flex items-center gap-2 text-sm font-medium text-amber-500">
+              <Lock size={20} />
+              Em breve
+            </span>
+          )}
+
+          <span className="text-xs font-bold rounded px-2 py-[0.125rem] text-white border border-teal-400">
+            {type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
+          </span>
+        </header>
+
+        <strong className="block mt-5 text-gray-200">{title}</strong>
+      </div>
+    </a>
+  );
+}
---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,52 @@
+import { gql, useQuery } from '@apollo/client';
+import { Lesson } from '@components';
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
+      id
+      slug
+      stage
+      title
+      availableAt
+      lessonType
+    }
+  }
+`;
+
+interface IGetLessonsQueryResponse {
+  lessons: {
+    id: string;
+    slug: string;
+    stage: string;
+    title: string;
+    availableAt: string;
+    lessonType: 'live' | 'class';
+  }[];
+}
+
+export default function Sidebar() {
+  const { data } = useQuery<IGetLessonsQueryResponse>(GET_LESSONS_QUERY);
+
+  return (
+    <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
+      <span className="block pb-6 mb-6 text-2xl font-bold border-b border-gray-600">
+        Cronograma de aulas
+      </span>
+
+      <div className="flex flex-col gap-8">
+        {data?.lessons.map((lesson) => {
+          return (
+            <Lesson
+              key={lesson.id}
+              title={lesson.title}
+              slug={lesson.slug}
+              availableAt={new Date(lesson.availableAt)}
+              type={lesson.lessonType}
+            />
+          );
+        })}
+      </div>
+    </aside>
+  );
+}
