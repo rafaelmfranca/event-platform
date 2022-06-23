ed45e2a22a4dd180884ba03c568fd6ac8260f27a
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-06-23 17:02:42 -0300

    feat: add redirect behavior in lesson card component

--- src/components/Lesson/index.tsx
+++ src/components/Lesson/index.tsx
@@ -1,6 +1,7 @@
 import { format, isPast } from 'date-fns';
 import ptBr from 'date-fns/locale/pt-BR';
 import { CheckCircle, Lock } from 'phosphor-react';
+import { Link } from 'react-router-dom';
 
 interface ILessonProps {
   title: string;
@@ -16,10 +17,10 @@ export default function Lesson({ title, slug, availableAt, type }: ILessonProps)
   });
 
   return (
-    <a href={slug}>
+    <Link to={`/event/lesson/${slug}`} className="group">
       <span className="text-gray-300">{availableDateFormatted}</span>
 
-      <div className="p-4 mt-2 border border-gray-500 rounded">
+      <div className="p-4 mt-2 border border-gray-500 rounded group-hover:border-teal-500">
         <header className="flex items-center justify-between">
           {isLessonAvailable ? (
             <span className="flex items-center gap-2 text-sm font-medium text-cyan-500">
@@ -33,13 +34,13 @@ export default function Lesson({ title, slug, availableAt, type }: ILessonProps)
             </span>
           )}
 
-          <span className="text-xs font-bold rounded px-2 py-[0.125rem] text-white border border-teal-400">
+          <span className="text-xs font-bold rounded px-2 py-[0.125rem] text-white border border-teal-500">
             {type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
           </span>
         </header>
 
         <strong className="block mt-5 text-gray-200">{title}</strong>
       </div>
-    </a>
+    </Link>
   );
 }
