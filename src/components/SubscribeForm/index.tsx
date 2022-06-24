c2045b0f7ef004a9093294fd526af1ea415db60c
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-06-24 16:54:46 -0300

    feat: add subscribe form component

---
+++ src/components/SubscribeForm/index.tsx
@@ -0,0 +1,48 @@
+import { CREATE_SUBSCRIBER_MUTATION } from '@/gql/mutations';
+import { useMutation } from '@apollo/client';
+import { FormEvent, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export default function SubscribeForm() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const navigate = useNavigate();
+
+  const [createSubscriber, { loading }] = useMutation(CREATE_SUBSCRIBER_MUTATION);
+
+  const handleSubscribe = async (e: FormEvent) => {
+    e.preventDefault();
+
+    await createSubscriber({ variables: { name, email } });
+
+    navigate('/event');
+  };
+
+  return (
+    <form action="" className="flex flex-col w-full gap-2" onSubmit={handleSubscribe}>
+      <input
+        placeholder="Seu nome completo"
+        className="px-5 bg-gray-900 border-2 border-gray-900 rounded h-14 focus:outline-none focus:border-teal-500"
+        type="text"
+        name={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        placeholder="Seu melhor e-mail"
+        className="px-5 bg-gray-900 border-2 border-gray-900 rounded h-14 focus:outline-none focus:border-teal-500"
+        type="email"
+        name={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <button
+        type="submit"
+        disabled={loading}
+        className="flex items-center justify-center w-full px-6 py-4 mt-4 text-sm font-bold uppercase transition-colors bg-teal-500 rounded hover:bg-teal-600 disabled:opacity-50"
+      >
+        Garantir minha vaga
+      </button>
+    </form>
+  );
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -5,6 +5,7 @@ import Header from './Header';
 import Lesson from './Lesson';
 import Logo from './Logo';
 import Sidebar from './Sidebar';
+import SubscribeForm from './SubscribeForm';
 import Video from './Video';
 
-export { Logo, Header, Sidebar, Video, Lesson, AnchorButton, Avatar, AnchorCard };
+export { Logo, Header, Sidebar, Video, Lesson, AnchorButton, Avatar, AnchorCard, SubscribeForm };
