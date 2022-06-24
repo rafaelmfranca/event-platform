eb2defc771e0ef250d518ed4e20b2983cf139c5d
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-06-24 16:58:51 -0300

    feat: add environment variables / auth header in apollo config

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,7 +1,10 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4ohzyj707e001xk7tyo58bk/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`,
+  },
   cache: new InMemoryCache(),
 });
 
